fda66a22a00ca5b8ca4eeae24cce89df23ccc147
kzjyddjq <1743165204@qq.com>
2019-08-22 09:35:47 +0800

    kzj

--- src/main/java/com/fjut/oj/edu/dao/DiscussDao.java
+++ src/main/java/com/fjut/oj/edu/dao/DiscussDao.java
@@ -1,9 +1,16 @@
 package com.fjut.oj.edu.dao;
 
 
+import com.fjut.oj.edu.model.Discuss;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DiscussDao {
+
+    List<Discuss> queryByAll();
+
+    void insert(Discuss discuss);
 
 }
---
+++ src/main/java/com/fjut/oj/edu/service/DiscussService.java
@@ -0,0 +1,7 @@
+package com.fjut.oj.edu.service;
+
+import java.util.List;
+
+public interface DiscussService {
+
+}
